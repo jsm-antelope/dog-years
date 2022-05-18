9765103b1554fa3a541c2ebe58552e0a63d03079
Thomas Gravdal <thomas@digvalley.com>
2022-05-18 13:40:54 +0200

    some cool stuff

---
+++ module.js
@@ -0,0 +1,26 @@
+import prompts from 'prompts'
+import chalk from 'chalk'
+;(async () => {
+  console.log('=================================')
+  console.log(chalk.green('This is a human-to-dog years calc'))
+  console.log('=================================')
+
+  const response = await prompts([
+    {
+      type: 'text',
+      name: 'name',
+      message: "What's your name?",
+    },
+    {
+      type: 'number',
+      name: 'age',
+      message: "What's your human age?",
+    },
+  ])
+  const dogAge = response.age * 10.5
+  const sentence = `My name is ${response.name}. I am ${response.age} years old in human years which is ${dogAge} years old in dog years.`
+
+  console.log('---------------------------------')
+  console.log(chalk.blue(sentence))
+  console.log('---------------------------------')
+})()
